096a6659defcfab4b28c84b392b72899328a11a9
dudwk814 <kj99658103@gmail.com>
2020-12-15 20:12:37 +0900

    2020-12-15 Chapter01

--- Chapter01/src/main/java/com/rubypaper/Chapter01/domain/BoardVO.java
+++ Chapter01/src/main/java/com/rubypaper/Chapter01/domain/BoardVO.java
@@ -1,7 +1,9 @@
 package com.rubypaper.Chapter01.domain;
 
-import java.util.Date;
+import lombok.Data;
 
+import java.util.Date;
+@Data
 public class BoardVO {
 
     private int seq;
@@ -11,51 +13,4 @@ public class BoardVO {
     private Date createDate = new Date();
     private int cnt = 0;
 
-    public int getSeq() {
-        return seq;
-    }
-
-    public void setSeq(int seq) {
-        this.seq = seq;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getWriter() {
-        return writer;
-    }
-
-    public void setWriter(String writer) {
-        this.writer = writer;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public Date getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(Date createDate) {
-        this.createDate = createDate;
-    }
-
-    public int getCnt() {
-        return cnt;
-    }
-
-    public void setCnt(int cnt) {
-        this.cnt = cnt;
-    }
 }
