cdd5789eaa4eab29d954b7b515f5c9ec8683553f
dudwk814 <kj99658103@gmail.com>
2020-12-15 21:27:10 +0900

    2020-12-15 chapter02

--- src/main/java/com/rubypaper/Chapter02/Chapter02Application.java
+++ src/main/java/com/rubypaper/Chapter02/Chapter02Application.java
@@ -3,6 +3,7 @@ package com.rubypaper.Chapter02;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/* Chapter02 */
 @SpringBootApplication
 public class Chapter02Application {
 
